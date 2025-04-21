e72026a67f298931259e2e605fe2ac60db40daf1
Udhul <oldengloe@gmail.com>
2025-04-21 11:42:37 +0200

    Update init to make CamBamProject directly available under the package name: cambam_builder

--- cambam_builder/__init__.py
+++ cambam_builder/__init__.py
@@ -1,4 +1,7 @@
 # Make this framework's directory (CamBam-Builder/cambam_builder/) a package of the CamBam-Builder project
 
+# Imports
+from .cambam_project import CamBamProject
+
 # Current package version
 __version__ = "0.1.0"
